2f1c3d25ce8b0fc67ddfb162f33e1147834e9f36
Akarsh <akarshmaurya19@gmail.com>
2022-10-15 21:50:05 +0530

    added numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -20,27 +20,27 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-//   if (numericFilters) {
-//     const operatorMap = {
-//       '>': '$gt',
-//       '>=': '$gte',
-//       '=': '$eq',
-//       '<': '$lt',
-//       '<=': '$lte',
-//     };
-//     const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-//     let filters = numericFilters.replace(
-//       regEx,
-//       (match) => `-${operatorMap[match]}-`
-//     );
-//     const options = ['price', 'rating'];
-//     filters = filters.split(',').forEach((item) => {
-//       const [field, operator, value] = item.split('-');
-//       if (options.includes(field)) {
-//         queryObject[field] = { [operator]: Number(value) };
-//       }
-//     });
-//   }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
 
   let result = Product.find(queryObject);
   // sort
@@ -51,15 +51,15 @@ const getAllProducts = async (req, res) => {
     result = result.sort('createdAt');
   }
 
-//   if (fields) {
-//     const fieldsList = fields.split(',').join(' ');
-//     result = result.select(fieldsList);
-//   }
-//   const page = Number(req.query.page) || 1;
-//   const limit = Number(req.query.limit) || 10;
-//   const skip = (page - 1) * limit;
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-//   result = result.skip(skip).limit(limit);
+  result = result.skip(skip).limit(limit);
   // 23
   // 4 7 7 7 2
 
